7aae3a6e1b9cdb6dc3bbfe992968bd1db49140f3
Jake Nelken <jnelks@gmail.com>
2015-06-01 00:18:56 -0400

    update README

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+alert("yo");
+var i=0;
+for (i=3; i<7; i++) {
+	document.getElementsByTagName("a")[i].innerHTML = "<img height=40px src='images/" + document.getElementsByTagName("a")[i].innerHTML + ".png'" + ">";
+};
+};
