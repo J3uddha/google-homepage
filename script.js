a39dee900488c7cafa13fa384c4317034225fcfb
Jake Nelken <jnelks@gmail.com>
2015-06-01 01:25:12 -0400

    update Vers 1.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-alert("yo");
+// alert("yo");
 var i=0;
 for (i=3; i<7; i++) {
 	document.getElementsByTagName("a")[i].innerHTML = "<img height=40px src='images/" + document.getElementsByTagName("a")[i].innerHTML + ".png'" + ">";
